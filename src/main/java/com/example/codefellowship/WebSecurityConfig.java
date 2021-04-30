4472ef6e44d866620e919a2393eb50426ef55101
anasswalha95 <anas.swalha@yahoo.com>
2021-04-30 03:10:44 +0300

    last update

--- src/main/java/com/example/codefellowship/WebSecurityConfig.java
+++ src/main/java/com/example/codefellowship/WebSecurityConfig.java
@@ -26,7 +26,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Override
-    protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
+    protected void configure( AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
     }
 
